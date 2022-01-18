34ba7929caad967d541cdcb04ecfd3da97a53b0b
Deepthi Srinidhi Rao <deepthi2609@gmail.com>
2022-01-17 16:39:37 -0800

    Squashed commit of the following:
    commit 6295a236cc962aeeac59f84cb81342db6270b407
    Merge: a060b77 c79d487
    Author: Deepthi Srinidhi Rao <deepthi2609@gmail.com>
    Date:   Mon Jan 17 16:37:11 2022 -0800
    
        Merge branch 'main' into Exercise_3.6
    
    commit a060b77fed379c662c782613e2b57db7078eaba7
    Author: Deepthi Srinidhi Rao <deepthi2609@gmail.com>
    Date:   Wed Jan 12 21:57:05 2022 -0800
    
        Display of list of movies and deleting fav movie
    
    commit d782a50f285ec480446a86d18bb6194653d94525
    Author: Deepthi Srinidhi Rao <deepthi2609@gmail.com>
    Date:   Tue Jan 11 16:14:30 2022 -0800
    
        User info, User update components
    
    commit 7173b3399d47863be9fe926d55a3e91f0bfbf2b0
    Author: Deepthi Srinidhi Rao <deepthi2609@gmail.com>
    Date:   Mon Jan 10 13:12:51 2022 -0800
    
        Director and genre component
    
    commit 77a877563f7bc2833aed6baa9f578d38b1fa11f7
    Author: Deepthi Srinidhi Rao <deepthi2609@gmail.com>
    Date:   Mon Jan 10 10:57:14 2022 -0800
    
        Routing added
    
    commit 2bc58644ebb8c9ace3cac582a05cbec7d9c33e8a
    Author: Deepthi Srinidhi Rao <deepthi2609@gmail.com>
    Date:   Sun Jan 9 15:59:51 2022 -0800
    
        Added login validations
    
    commit e3d1428799e09091c291587316b20106d626d1c2
    Author: Deepthi Srinidhi Rao <deepthi2609@gmail.com>
    Date:   Mon Jan 3 16:11:43 2022 -0800
    
        modified navbar style
    
    commit a808b9e458b02c195d5c510e7aac0e1c6c8f5cc9
    Author: Deepthi Srinidhi Rao <deepthi2609@gmail.com>
    Date:   Mon Jan 3 16:02:49 2022 -0800
    
        Styling added using react-bootstrap

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { API_URL } from '../../utils/constant';
+import { validate } from '../../utils/validate';
+import { Card, 
+         Form, 
+         Button } from 'react-bootstrap';
+
 import './registration-view.scss';
+
 
 const RegisterationView = () => {
     const [userData, setUserData] = React.useState({
@@ -9,56 +16,126 @@ const RegisterationView = () => {
         email: '',
         dob: ''
     });
-    
+    const [errors, setErrors] = useState({});
+    const [message, setMessage] = useState("");
+
     const handleChange = e => {
         const {name, value} = e.target;
+        let error = validate(name, value);
+        setErrors((prevErr) =>{
+            return {
+                ...prevErr,
+                ...error
+            }
+        });
+
         setUserData((prevState) =>({
             ...prevState,
-            [name] : value,
+            [name] : value
         }));
     }
 
     const handleSubmit = e => {
         e.preventDefault();
-        console.log(userData);
+        console.log(userData, new Date(userData.dob));
+        const { username, password, email, dob } = userData; 
+
+        axios.post(`${API_URL}/users`,{
+            Username: username,
+            Password: password,
+            Email: email,
+            Birthday: new Date(dob)
+        })
+        .then(response =>{
+            const data = response.data;
+            setMessage("User registration was successful");
+            setUserData({
+                username : '',
+                password: '',
+                email: '',
+                dob: ''
+            });
+            data && window.open('/', '_self'); 
+         })
+         .catch(error =>{
+            setMessage("User registration failed. Plaese try later");
+            /*window.open('/', '_self');*/
+            console.log(`Registration failed!!! ${error}`);
+         });
     }
 
     return (
-        <>
-            <h1>User Registration Page</h1>
-            <form>
-                <label htmlFor="username">Username: </label>
-                <input type="text" 
-                    name="username"
-                    placeholder="Enter username" 
-                    onChange={handleChange} 
-                    required />
-                <input 
-                    type="email" 
-                    name="email" 
-                    placeholder="Enter email"
-                    onChange={handleChange} 
-                    required />                
-                <label htmlFor="password">Password: </label>
-                <input 
-                    type="password" 
-                    name="password" 
-                    placeholder="Enter password"
-                    onChange={handleChange} 
-                    minLength={5}
-                    required />
-                <label htmlFor="email">Email: </label>
-                
-                <label htmlFor="dob">DOB: </label>
-                <input 
-                    type="date" 
-                    name="dob"
-                    placeholder="Enter date of birth" 
-                    onChange={handleChange} 
-                    required />
-                <button type="submit" onClick={handleSubmit}>Register</button>
-            </form>
-        </>
+        <Card className="reg-container">
+            <Card.Body>
+                <Card.Title as="h2" className="text-center">Registration Page</Card.Title>
+                {message && (
+                        <div className="form-group">
+                            <div className="alert alert-danger my-1 py-2" role="alert">
+                                {message}
+                            </div>
+                        </div>
+                )}
+                <Form>
+                    <Form.Group controlId="username" className="mb-3">
+                        <Form.Label>Username: </Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            name="username" 
+                            value={userData.username || ''}
+                            onChange={handleChange}
+                            placeholder="Enter username here"
+                            isInvalid={!!errors.username}
+                            required />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.username}
+                        </Form.Control.Feedback>
+                    </Form.Group>                           
+                    <Form.Group controlId="password" className="mb-3"> 
+                        <Form.Label>Password: </Form.Label> 
+                        <Form.Control 
+                            type="password" 
+                            name="password" 
+                            value={userData.password || ''}
+                            onChange={handleChange}
+                            placeholder="Enter password here"
+                            isInvalid={!!errors.password}
+                            required />                       
+                        <Form.Control.Feedback type="invalid">
+                            {errors.password}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group controlId="email" className="mb-3"> 
+                        <Form.Label>Email: </Form.Label> 
+                        <Form.Control 
+                            type="email" 
+                            name="email" 
+                            value={userData.email || ''}
+                            onChange={handleChange}
+                            placeholder="Enter email here"
+                            isInvalid={!!errors.email}
+                            required />                       
+                        <Form.Control.Feedback type="invalid">
+                            {errors.email}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group controlId="dob" className="mb-3"> 
+                        <Form.Label>Date of Birth: </Form.Label> 
+                        <Form.Control 
+                            type="date"
+                            name="dob" 
+                            onChange={handleChange}
+                            placeholder="Enter date of birth here"/>
+                    </Form.Group>
+                    <Button 
+                        type="submit"
+                        className="login-btn"
+                        disabled={!userData.email && !userData.password}
+                        onClick={handleSubmit}>
+                            Register
+                    </Button>
+                </Form>
+            </Card.Body>
+        </Card>
     );
 }
 
